08c06134a48ce20b3d593e2167e5bb3e85619baf
Sean Armstrong <seanmaarmstrong@gmail.com>
2023-08-15 20:18:07 -0700

    added navbar layout to header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 
 function Header() {
-    return (
-        <div>
-            <header>Header</header>
-        </div>
-    )
+ return (
+   <header>
+    <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
+      <Container>
+        <Navbar.Brand href="/">ELCAUDILLO</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/cart">Cart</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+   </header>
+  )
 }
 
 export default Header
