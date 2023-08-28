0d0a3f935c99e357e05bc0a236c499b7a9404017
Sean Armstrong <seanmaarmstrong@gmail.com>
2023-08-28 10:30:47 -0700

    imported some libraries

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
 function ProductScreen() {
+//function ProductScreen({ match }) {
+    // const product = products.find((p) => p._id == props.match.params.id)
     return (
         <div>
             Product
