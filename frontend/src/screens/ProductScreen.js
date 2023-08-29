fdfce4d1d5eec33fe12924ff6baa8d8e25ffeff2
Sean Armstrong <seanmaarmstrong@gmail.com>
2023-08-28 17:13:10 -0700

    added producted name, ratings, and reviews with formatting to ProductScreen.js

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -11,10 +11,27 @@ function ProductScreen({ match }) {
     return (
         <div>
             <Link to='/' className='btn btn-light my-3'>Go Back</Link>
+
             <Row>
                 <Col md={6}>
                     <Image src={product.image} alt={product.name} fluid />
                 </Col>
+
+                <Col md={3}>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>
+                            <h3>{product.name}</h3>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <Rating value={product.rating} text ={`${product.numReviews} reviews`} color={'#f8e825'} />
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+
+                {/* <Col md={3}> */}
+                   {/* </Col> */}
+                {/* </Col> */}
             </Row>
         </div>
     )
