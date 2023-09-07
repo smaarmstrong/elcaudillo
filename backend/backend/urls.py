e488949446c36d5057f23e672742050cb4eed530
Sean Armstrong <seanmaarmstrong@gmail.com>
2023-09-06 20:45:47 -0700

    added urlpatterns += static thing for later

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,3 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
