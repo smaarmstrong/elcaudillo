236f34acbe1e13f368cbe1c6f43889c77ebf57ae
Sean Armstrong <seanmaarmstrong@gmail.com>
2023-09-08 22:21:23 -0700

    added user field to products models.py

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+# built-in user model
+from django.contrib.auth.models import User
+
 # Create your models here.
+class Product(models.Model)
+
+# models.ForeignKey: field type to establish a relationship between two models
+# User: the model reference; built-in with Django
+#on_delete=models.SET_NULL: if the user is deleted, set the field to null
+# null=FALSE: null is not allowed in the database
+user = models.ForeignKey(User, on_delete=models.SET_NULL, null=False)
