85664be574d00575e31daeb1c628c96764a8d83e
Sean Armstrong <seanmaarmstrong@gmail.com>
2023-09-08 22:30:12 -0700

    added name, brand, and category fields to the products model.py

--- backend/products/models.py
+++ backend/products/models.py
@@ -11,3 +11,11 @@ class Product(models.Model)
 #on_delete=models.SET_NULL: if the user is deleted, set the field to null
 # null=FALSE: null is not allowed in the database
 user = models.ForeignKey(User, on_delete=models.SET_NULL, null=False)
+
+# models.CharField: field type from Django ORM to store medium length strings
+# max_length=200: maximum length of strings set to 200
+# null=True: null is permitted to be stored in the database
+# blank=True: the field is allowed to be empty
+name = models.CharField(max_length=200, null=True, blank=True)
+brand = models.CharField(max_length=200, null=True, blank=True)
+category = models.CharField(max_length=200, null=True, blank=True)
