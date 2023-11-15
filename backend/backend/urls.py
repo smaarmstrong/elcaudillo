e71daf07dbf20581b70a90dce65ae666ed853e93
Sean Armstrong <seanmaarmstrong@gmail.com>
2023-11-15 10:40:11 +0000

    fixed senseless urls.py mistake for collecting default django admin files

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('api/products/<str:pk>', product_views.getProduct, name="product"),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
